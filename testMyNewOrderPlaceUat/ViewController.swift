0a513e4f24efc988643ef732498c17c0df1b0c27
风筝 <13286953452@126.com>
2018-11-22 18:10:57 +0800

    Initial Commit

---
+++ testMyNewOrderPlaceUat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testMyNewOrderPlaceUat
+//
+//  Created by 陈培爵 on 2018/11/22.
+//  Copyright © 2018年 PeiJueChen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
